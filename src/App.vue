f27edf12dbe242efc708a3bad8b61eef825ae5d0
AnoirTaktak <anoirtaktak@hotmail.fr>
2024-02-27 17:28:56 +0100

    first vue project creation State + Props + Hooks Learning

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+//import Listlivres from "./components/Listlivres.vue"
+//import Inscription from "./components/Inscription.vue"
+import Facture from "./components/Facture.vue";
+</script>
+<template>
+<Facture/>
+  
+</template>
+<style scoped>
+</style>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.css';
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+const facture =ref({
+    numfact:"100",
+    datefact:"18/05/2022",
+    raisoc:"Confort-Plus",
+    responsable:"Turki Mounir"
+    });
+    const articles=ref([
+    {reference:"1",designation:"la vaisselle",qte:3,prix:1500},
+    {reference:"3",designation:"Machine a laver",qte:3,prix:1200},
+    {reference:"17",designation:"Micro-onde",qte:4,prix:300}
+    ]);
